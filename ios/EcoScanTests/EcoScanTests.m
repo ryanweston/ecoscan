5c5508fc5e790ab46c77fd3143f94264e01d91f0
Ryan Weston <ryanlweston@gmail.com>
2021-12-09 16:24:45 +0000

    chore(app): change project name to EcoScan

--- ios/EcoScanTests/EcoScanTests.m
+++ ios/EcoScanTests/EcoScanTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NetscapesTests : XCTestCase
+@interface EcoScanTests : XCTestCase
 
 @end
 
-@implementation NetscapesTests
+@implementation EcoScanTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
