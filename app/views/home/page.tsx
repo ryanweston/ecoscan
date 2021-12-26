d1a261036c79782fbb87316d6fa31f411d370619
Ryan Weston <ryanlweston@gmail.com>
2021-12-26 10:53:13 +0000

    chore(app): begin fixing render loop issues

--- app/views/home/page.tsx
+++ app/views/home/page.tsx
@@ -1,54 +1,61 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback, useContext} from 'react';
+import {Text} from 'react-native';
 import {Headline, Container} from '../../components';
 import ProductItem from '../../components/product-item'; // Move to relevant place later
 import {request} from '../../request';
 import {AuthContext} from '../../auth/auth-provider';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const Home = ({navigation}: any) => {
-  const {handleUnauthorized}: any = React.useContext(AuthContext);
+  const {handleUnauthorized}: any = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState([]);
+
+  const getProducts = useCallback(async () => {
+    try {
+      const response = await request.get('/products/most-popularss');
+      setProducts(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      handleUnauthorized(error.response.status);
+    }
+  }, [handleUnauthorized]);
 
   useEffect(() => {
     getProducts();
-  });
+  }, [getProducts]);
 
-  const headerTitle = 'Recently scanned';
+  const headerTitle = 'Most popular';
   const headerTitle1 = 'Featured';
 
-  const getProducts = async () => {
-    try {
-      const response = await request.get('/products');
-      setProducts(response.data);
-    } catch (error) {
-      handleUnauthorized(error.response.status);
-    }
-  };
-
   return (
-    <Container>
-      <Headline>{headerTitle}</Headline>
-      {products.length > 0 ? (
-        products.map((item, index) => {
-          return (
-            <ProductItem key={index} navigation={navigation} info={item} />
-          );
-        })
-      ) : (
-        <ProductItem />
-      )}
+    <ScrollView>
+      <Container>
+        <Headline>{headerTitle}</Headline>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          products.map((item, index) => {
+            return (
+              <ProductItem key={index} navigation={navigation} info={item} />
+            );
+          })
+        )}
 
-      <Headline>{headerTitle1}</Headline>
-      {products.length > 0 ? (
-        products.map((item, index) => {
-          //@ts-ignore
-          return (
-            <ProductItem key={index} navigation={navigation} info={item} />
-          );
-        })
-      ) : (
-        <ProductItem />
-      )}
-    </Container>
+        <Headline propStyles={{marginTop: 20}}>{headerTitle1}</Headline>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          products.map((item, index) => {
+            //@ts-ignore
+            return (
+              <ProductItem key={index} navigation={navigation} info={item} />
+            );
+          })
+        )}
+      </Container>
+    </ScrollView>
   );
 };
 
