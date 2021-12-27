1cf1bbb59c27377099232954123f576f6fa61638
Ryan Weston <ryanlweston@gmail.com>
2021-12-27 11:00:27 +0000

    feat(app): lint + styles + progress bar

--- app/views/product/scanner/product-modal.tsx
+++ app/views/product/scanner/product-modal.tsx
@@ -1,29 +1,50 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-native-modal';
 import {
   Text,
-  Button,
-  Image,
   ScrollView,
   View,
   Pressable,
   StyleSheet,
 } from 'react-native';
-import {Container, Headline, ProductScore} from '../../../components';
-import {request} from '../../../request';
+// @ts-ignore
+import ProgressBar from 'react-native-progress/Bar';
+import { Container, Headline, ProductScore } from '@/components';
+import { request } from '@/request';
+import { withTheme } from '@/styles/theme-context';
 
-const ProductModal = ({barcode, setBarcode}: any) => {
+const styles = StyleSheet.create({
+  button: {
+    padding: 20,
+    color: 'white',
+    backgroundColor: '#648142',
+    borderRadius: 100,
+  },
+  scoreContainer: {
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginRight: 'auto',
+    alignItems: 'center',
+    paddingBottom: 20,
+  },
+  listItem: {
+    marginTop: 40,
+    marginBottom: 40,
+    width: '100%',
+    flexDirection: 'row',
+  },
+});
+
+function ProductModal({ barcode, setBarcode, theme }: any) {
   const [loading, setLoading] = useState(true);
   const [product, setProduct] = useState({});
 
-  useEffect(() => {
-    console.log('PRODUCT MODAL BARCODE', barcode);
-    getProduct();
-  }, []);
+  const colours = theme.currentTheme;
 
   const getProduct = async () => {
     try {
-      const response = await request.get('/products/?barcode=' + barcode);
+      const response = await request.get(`/products/?barcode=${barcode}`);
       setProduct(response.data);
       setLoading(false);
     } catch (e) {
@@ -31,7 +52,13 @@ const ProductModal = ({barcode, setBarcode}: any) => {
     }
   };
 
-  let isVisible = !!barcode;
+  useEffect(() => {
+    console.log('PRODUCT MODAL BARCODE', barcode);
+    console.log('MODAL:', theme);
+    getProduct();
+  }, []);
+
+  const isVisible = !!barcode;
 
   // Close this modal in a more native way using the correct props
   return (
@@ -41,7 +68,8 @@ const ProductModal = ({barcode, setBarcode}: any) => {
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-      }}>
+      }}
+    >
       <Modal
         swipeDirection={['down']}
         onSwipeComplete={() => setBarcode('')}
@@ -50,21 +78,24 @@ const ProductModal = ({barcode, setBarcode}: any) => {
           justifyContent: 'flex-end',
           margin: 0,
         }}
-        animationIn={'slideInUp'}
-        isVisible={isVisible}>
+        animationIn="slideInUp"
+        isVisible={isVisible}
+      >
         <View
           style={{
             backgroundColor: 'white',
             borderTopLeftRadius: 30,
             borderTopRightRadius: 30,
-          }}>
+          }}
+        >
           <View
             style={{
               marginTop: 15,
               width: '100%',
               flexDirection: 'column',
               alignItems: 'center',
-            }}>
+            }}
+          >
             <Pressable
               style={{
                 width: 100,
@@ -84,31 +115,46 @@ const ProductModal = ({barcode, setBarcode}: any) => {
                   flexDirection: 'column',
                   alignItems: 'center',
                   justifyContent: 'center',
-                }}>
+                }}
+              >
                 <Headline>Seaching for item...</Headline>
               </View>
             ) : product ? (
               <ScrollView>
                 <View style={styles.scoreContainer}>
-                  <ProductScore score={product.reviewAggregate} large={true} />
+                  <ProductScore score={product.reviewAggregate} large />
                 </View>
                 <Headline>{product.productName}</Headline>
                 <Text>{product.brand.name}</Text>
-                <Image
+                <View style={styles.listItem}>
+                  <Text>Sustainability:</Text>
+                  <ProgressBar
+                    progress={0.3}
+                    width={200}
+                    height={15}
+                    borderRadius={40}
+                    unfilledColor="#DDD"
+                    borderWidth={0}
+                    color={colours.accent}
+                  />
+                </View>
+                {/* <Image
                   style={{
                     width: 200,
                     height: 200,
                     marginTop: 20,
                     borderRadius: 50,
                   }}
-                  source={{uri: product.img}}
-                />
-                <Button
-                  title="Add a review"
+                  source={{ uri: product.img }}
+                /> */}
+                <Pressable
+                  style={styles.button}
                   onPress={() => {
                     setBarcode('');
                   }}
-                />
+                >
+                  <Text style={{ color: colours.secondary }}>Add a review</Text>
+                </Pressable>
               </ScrollView>
             ) : (
               <View
@@ -116,7 +162,8 @@ const ProductModal = ({barcode, setBarcode}: any) => {
                   flexDirection: 'column',
                   alignItems: 'center',
                   justifyContent: 'center',
-                }}>
+                }}
+              >
                 <Headline>Sorry, we couldn't find this item.</Headline>
               </View>
             )}
@@ -125,17 +172,6 @@ const ProductModal = ({barcode, setBarcode}: any) => {
       </Modal>
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  scoreContainer: {
-    width: '100%',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    marginRight: 'auto',
-    alignItems: 'center',
-    paddingBottom: 20,
-  },
-});
-
-export default ProductModal;
+export default withTheme(ProductModal);
