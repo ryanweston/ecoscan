aaf6062b9e8c52c506211d9d006af7bc7baee87a
Ryan Weston <ryanlweston@gmail.com>
2021-12-27 08:22:45 +0000

    chore(app): configure eslint style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,56 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+    // "env": {
+    //     "es2021": true
+    // },
+    root: true,
+    extends: [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            "jsx": true
+        },
+        ecmaVersion: 13,
+        sourceType: "module"
+    },
+    plugins: [
+        "react",
+        "@typescript-eslint"
+    ],
+    rules: {
+    }
+}
+
+// {
+//   "extends": [
+//   "airbnb",
+//   "prettier",
+//   "prettier/react",
+//   "plugin:prettier/recommended",
+//   "eslint-config-prettier"
+//   ],
+//   "env": {
+//     "es2021": true
+// },
+//   "parser": "babel-eslint",
+//   "rules": {
+//   "import/no-unresolved": "off",
+//   "react/jsx-filename-extension": [
+//   1,
+//   {
+//   "extensions": [".js", ".jsx"]
+//   }
+//   ],
+//   "prettier/prettier": [
+//   "error",
+//   {
+//   "trailingComma": "es5",
+//   "singleQuote": true,
+//   "printWidth": 100
+//   }
+//   ]
+//   },
+//   "plugins": ["prettier"]
+//   }
