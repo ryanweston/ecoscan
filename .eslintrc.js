f2364fbfd2a1e8d961d3c9f7bc1732747133fb4f
Ryan Weston <ryanlweston@gmail.com>
2021-12-27 09:05:47 +0000

    feat(app): config eslint & tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,33 @@
 module.exports = {
-    // "env": {
-    //     "es2021": true
-    // },
-    root: true,
-    extends: [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            "jsx": true
-        },
-        ecmaVersion: 13,
-        sourceType: "module"
+  // "env": {
+  //     "es2021": true
+  // },
+  root: true,
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: [
-        "react",
-        "@typescript-eslint"
-    ],
-    rules: {
-    }
-}
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 0,
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    // Fix error thrown with TSX files
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+  },
+};
 
 // {
 //   "extends": [
