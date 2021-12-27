b4605c902233ba684c6d7e65bcd882663226c2fb
Ryan Weston <ryanlweston@gmail.com>
2021-12-27 09:04:34 +0000

    refactor(app): style experiment on product page

--- app/views/product/product-page.tsx
+++ app/views/product/product-page.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {Container, Headline} from '../../components';
-import {Text, View, Button} from 'react-native';
+import {Container, Headline, ProductScore} from '../../components';
+import {Text, View, Button, StyleSheet} from 'react-native';
 import {request} from '../../request';
 
 interface Product {
@@ -14,32 +14,61 @@ interface Product {
 
 const ProductPage = ({navigation, route}: any) => {
   const [item, setItem] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const {barcode} = route.params;
 
   useEffect(() => {
+    console.log('PRODUCT PAGE RENDER');
     getProduct(barcode);
   }, [barcode]);
 
   const getProduct = async (barcode: string) => {
     try {
+      setIsLoading(true);
       const response = await request.get('/products/?barcode=' + barcode);
       console.log(response.data);
       setItem(response.data);
+      setIsLoading(false);
     } catch (e) {
+      setIsLoading(false);
       console.log(e);
     }
   };
 
   return (
     <Container>
-      {item ? (
+      {isLoading ? (
+        <Text>Loading</Text>
+      ) : (
         <View>
+          <View style={styles.scoreContainer}>
+            <ProductScore score={item.reviewAggregate} large={true} />
+          </View>
           <Headline>{item.productName}</Headline>
-          <Headline>{item.brandName}</Headline>
-          <Text>Brand: {item.brandRating}</Text>
-          <Text>Quality: {item.productQuality}</Text>
-          <Text>Rating: {item.productRating}</Text>
-          <Headline>Total score: {item.totalScore}</Headline>
+          <Headline>
+            {item.brand.name ? item.brand.name : 'No brand available'}
+          </Headline>
+          {item.reviewAggregate ? (
+            <View>
+              <Headline>Quality: {item.reviewAggregate.qualityScore}</Headline>
+              <Headline>
+                Sustainability: {item.reviewAggregate.sustainabilityScore}
+              </Headline>
+            </View>
+          ) : (
+            <Text>No scores yet</Text>
+          )}
+
+          <Headline style={{marginTop: 30}}>Brand:</Headline>
+          {item.brand.qualityScore !== 'NaN' ? (
+            <View>
+              <Text>Quality: {item.brand.qualityScore}</Text>
+              <Text>Sustainability: {item.brand.sustainabilityScore}</Text>
+            </View>
+          ) : (
+            <Text>No brand information yet</Text>
+          )}
+
           <Button
             title="How do we evaluate scores?"
             onPress={() => {
@@ -53,11 +82,20 @@ const ProductPage = ({navigation, route}: any) => {
             }}
           />
         </View>
-      ) : (
-        <Text>Loading</Text>
       )}
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  scoreContainer: {
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginRight: 'auto',
+    alignItems: 'center',
+    paddingBottom: 20,
+  },
+});
 
 export default ProductPage;
